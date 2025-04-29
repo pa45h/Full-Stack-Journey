b5132f6574ee9ae0a4489c34ff31b41234be9f87
Parth Katariya <parth.katariya87@gmail.com>
2025-04-30 00:24:13 +0530

    BusinessNameGenerator

--- 04_JAVASCRIPT/Exercises/BusinessNameGenerator.js
+++ 04_JAVASCRIPT/Exercises/BusinessNameGenerator.js
@@ -2,27 +2,29 @@
 //  and generate random business names
 
 let adjectives = {
-    1:"Stellar",
-    2:"Rapid",
-    3:"Quantum",
-    4:"Agile",
-    5:"Bright"
-}
+  1: "Stellar",
+  2: "Rapid",
+  3: "Quantum",
+  4: "Agile",
+  5: "Bright",
+};
 let topics = {
-    1:"Tech",
-    2:"Finance",
-    3:"Health",
-    4:"Media",
-    5:"Energy"
-}
+  1: "Tech",
+  2: "Finance",
+  3: "Health",
+  4: "Media",
+  5: "Energy",
+};
 let others = {
-    1:"Solutions",
-    2:"Hub",
-    3:"Works",
-    4:"Labs",
-    5:"Nexus"
-}
+  1: "Solutions",
+  2: "Hub",
+  3: "Works",
+  4: "Labs",
+  5: "Nexus",
+};
 
-console.log(`${adjectives[Math.round(Math.random()*4 + 1)]} ${topics[Math.round(Math.random()*4 + 1)]} ${others[Math.round(Math.random()*4 + 1)]}`);
-
-
+console.log(
+  `${adjectives[Math.round(Math.random() * 4 + 1)]} ${
+    topics[Math.round(Math.random() * 4 + 1)]
+  } ${others[Math.round(Math.random() * 4 + 1)]}`
+);
