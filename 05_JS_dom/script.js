f867d2947fc7563dccfdcc94ba8b843f7ce339d8
Parth Katariya <parth.katariya87@gmail.com>
2025-04-29 12:33:50 +0530

    JS_dom

--- 05_JS_dom/script.js
+++ 05_JS_dom/script.js
@@ -5,17 +5,3 @@ console.log(window);                // global window object
 console.log(window.document);       // html document
 // OR console.log(document);
 
-document.getElementById('nav'); 
-document.getElementById('h1').id;
-document.getElementById('h1').getAttribute('class');
-document.getElementById('h1').setAttribute('class','h1Class');
-
-// const myDiv = document.getElementById('div');
-// myDiv.style.backgroundColor='green';
-// myDiv.style.color='white';
-
-const myPara = document.getElementById('para');
-myPara.innerHTML;
-// '\n                This is h3 inside p of main\n            '
-myPara.outerHTML;
-// '<h3 id="h3" style="background-color: green; color: white;">\n                This is h3 inside p of main\n            </h3>'
