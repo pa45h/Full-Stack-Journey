871228965bd16af3e1e0cb0eaa6442c11647ca7e
Parth Katariya <parth.katariya87@gmail.com>
2025-06-09 19:38:31 +0530

    CustomReact

--- 06_React_JS/01_DemoReactVite/demo-react-vite-app/src/App.jsx
+++ 06_React_JS/01_DemoReactVite/demo-react-vite-app/src/App.jsx
@@ -1,8 +1,11 @@
 function App() {
+  const userName = "pa45h";
+
   return (
   <>
     <h1>Hello React Vite App</h1>
     <h2>created using : vite</h2>
+    <h3>User Name : {userName}</h3>
   </>
   );
 }
--- 06_React_JS/01_DemoReactVite/demo-react-vite-app/src/main.jsx
+++ 06_React_JS/01_DemoReactVite/demo-react-vite-app/src/main.jsx
@@ -1,11 +1,37 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import Hello from "./components/First.jsx";
+import React from "react";
+
+const myElement = (
+  <a href="https://react.dev/" target="_blank">
+    react.org (custom element)
+  </a>
+);
+
+function funcElement() {
+  return (
+    <div>
+      <h3>Custom element using function</h3>
+    </div>
+  );
+}
+
+const anotherChild = " (custom react element)";
+const customReactElement = React.createElement(
+  "a",
+  { href: "https://react.dev/", target: "_blank" },
+  "react.org",
+  anotherChild
+);
+
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <>
     <App />
     <Hello />
-  </StrictMode>
+    {myElement}
+    {funcElement()}
+    {customReactElement}
+  </>
 );
