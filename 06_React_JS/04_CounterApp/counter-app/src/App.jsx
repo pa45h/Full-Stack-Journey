d3673efa06268cda0612ae74a4ec9094bbafc1d5
Parth Katariya <parth.katariya87@gmail.com>
2025-06-11 12:41:00 +0530

    CounterApp

---
+++ 06_React_JS/04_CounterApp/counter-app/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const addValue = () => {
+    setCount(count + 1);
+  };
+
+  const rmvValue = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <>
+      <h1>Counter App</h1>
+      <h2>{count}</h2>
+      <button onClick={rmvValue}>{count}-1</button>
+      <button onClick={addValue}>{count}+1</button>
+    </>
+  );
+}
+
+export default App;
