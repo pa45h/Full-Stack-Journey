abb0cd7093e12afe494f64397a388b64c8b2379b
Parth Katariya <parth.katariya87@gmail.com>
2025-06-19 12:28:20 +0530

    CounterApp

--- 06_React_JS/04_CounterApp/counter-app/src/App.jsx
+++ 06_React_JS/04_CounterApp/counter-app/src/App.jsx
@@ -12,11 +12,16 @@ function App() {
     setCount(count - 1);
   };
 
+  const resetValue = () => {
+    setCount(0);
+  };
+
   return (
     <>
       <h1>Counter App</h1>
       <h2>{count}</h2>
       <button onClick={rmvValue}>{count}-1</button>
+      <button onClick={resetValue}>RESET</button>
       <button onClick={addValue}>{count}+1</button>
     </>
   );
