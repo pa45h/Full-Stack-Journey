fbb8b4eb6a81ff1b717696844e0028968d8935ab
Parth Katariya <parth.katariya87@gmail.com>
2025-06-25 00:58:31 +0530

    RandomGifsApp

--- 06_React_JS/12_RandomGifs/random-gifs-app/src/App.jsx
+++ 06_React_JS/12_RandomGifs/random-gifs-app/src/App.jsx
@@ -3,7 +3,7 @@ import SearchedGif from "./components/SearchedGif";
 
 function App() {
   return (
-    <div className="w-screen h-screen bg-gray-950">
+    <div className="w-screen h-screen bg-gray-950 flex flex-col gap-5 justify-center">
       <div className="flex justify-center text-center">
         <span className="absolute mx-auto py-4 flex border w-fit bg-gradient-to-r blur-xl from-blue-500 via-teal-500 to-pink-500 bg-clip-text text-6xl box-content font-extrabold text-transparent text-center select-none">
           Random Gifs
@@ -13,7 +13,7 @@ function App() {
         </h1>
       </div>
 
-      <div className="flex justify-evenly items-center">
+      <div className="flex justify-evenly items-center flex-wrap gap-5">
         <RandomGif />
         <SearchedGif />
       </div>
