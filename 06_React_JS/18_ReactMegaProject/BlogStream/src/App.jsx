3235f458932f72607261dcf2bddd2849f5be548b
Parth Katariya <parth.katariya87@gmail.com>
2025-06-30 23:14:04 +0530

    BlogStream store

--- 06_React_JS/18_ReactMegaProject/BlogStream/src/App.jsx
+++ 06_React_JS/18_ReactMegaProject/BlogStream/src/App.jsx
@@ -1,11 +1,38 @@
-function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService, { AuthService } from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router";
 
-  return (
-    <>
-      <h1>BlogStream</h1>
-    </>
-  );
+function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-center bg-gray-900 text-white">
+      <div className="w-full block">
+        <Header />
+          <main>
+            {/* <Outlet/> */}
+          </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
