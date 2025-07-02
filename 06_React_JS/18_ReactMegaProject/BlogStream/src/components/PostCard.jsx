7408cb3277a966b15798488dc9bb92f885c4ccee
Parth Katariya <parth.katariya87@gmail.com>
2025-07-02 23:45:32 +0530

    BlogStream components

---
+++ 06_React_JS/18_ReactMegaProject/BlogStream/src/components/PostCard.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Service from "../appwrite/db";
+import { Link } from "react-router";
+
+function PostCard({ $id, title, featuredImage }) {
+  return (
+    <Link to={`/post/${$id}`}>
+      <div className="w-full bg-gray-200 rounded-xl p-4">
+        <div className="w-full justify-center mb-4">
+          <img
+            src={Service.getFilePreview(featuredImage)}
+            alt={title}
+            className="rounded-xl"
+          />
+        </div>
+        <h2 className="text-xl font-bold">{title}</h2>
+      </div>
+    </Link>
+  );
+}
+
+export default PostCard;
