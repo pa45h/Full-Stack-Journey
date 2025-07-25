139e7eed887a8b1aa95dd4685e66ceafcc39b04b
Parth Katariya <parth.katariya87@gmail.com>
2025-07-25 13:38:38 +0530

    FilesApp

--- 07_BackEnd/05_FilesApp/models/File.model.js
+++ 07_BackEnd/05_FilesApp/models/File.model.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
 const fileSchema = mongoose.Schema({
   name: {
@@ -14,6 +16,31 @@ const fileSchema = mongoose.Schema({
   email: {
     type: String,
   },
+});
+
+fileSchema.post("save", async (doc) => {
+  try {
+    console.log("Doc ->", doc);
+
+    let transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+    });
+
+    let info = await transporter.sendMail({
+      from: `Parth Katariya`,
+      to: doc.email,
+      subject: "New File Uploaded On Cloudinary!",
+      html: `<h2>Hello Mr.${doc.name},</h2> <p>Your File Uploaded Successfully On Cloudinary!</p> <p>Here Is The File: ${doc.url}</p>`,
+    });
+
+    console.log(info);
+  } catch (error) {
+    console.error(error.message);
+  }
 });
 
 module.exports = mongoose.model("File", fileSchema);
