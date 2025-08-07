ba1fb98c6eb12ea828aee98d650107182e8ebf90
Parth Katariya <parth.katariya87@gmail.com>
2025-08-07 23:51:45 +0530

    Navbar v2

--- 08_FullStackProject-1/EduStream/Client/src/components/common/Navbar.jsx
+++ 08_FullStackProject-1/EduStream/Client/src/components/common/Navbar.jsx
@@ -54,7 +54,7 @@ const Navbar = () => {
                       <p className="text-richblack-500">{link.title}</p>
                       <IoMdArrowDropdown className="text-richblack-500" />
 
-                      <div className="invisible absolute left-[50%] top-[50%] flex flex-col rounded-md bg-richblack-5 text-richblack-900 opacity-0 transition-all duration-200 group-hover:visible group-hover:opacity-100 lg:w-[300px] translate-x-[-50%] translate-y-[20%] z-50 justify-center p-4 group text-[15px] font-medium">
+                      <div className="invisible absolute left-[50%] top-[50%] flex flex-col rounded-md bg-richblack-5 text-richblack-900 opacity-0 transition-all duration-200 group-hover:visible group-hover:opacity-100 lg:w-[300px] translate-x-[-50%] translate-y-[12%] z-50 justify-center p-4 group text-[15px] font-medium">
                         <div className="absolute left-[50%] top-0 h-6 w-6 rotate-45 rounded bg-richblack-5 translate-y-[-30%] translate-x-[85%] -z-10"></div>
                         {subLinks ? (
                           subLinks.map((subLink, index) => (
