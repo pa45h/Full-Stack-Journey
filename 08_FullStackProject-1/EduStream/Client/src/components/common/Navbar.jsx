173b8a546f937c3ddbe41ed5c7121f8bc92dbe05
Parth Katariya <parth.katariya87@gmail.com>
2025-09-27 22:53:29 +0530

    navbar ux

--- 08_FullStackProject-1/EduStream/Client/src/components/common/Navbar.jsx
+++ 08_FullStackProject-1/EduStream/Client/src/components/common/Navbar.jsx
@@ -8,14 +8,18 @@ import { GiShoppingCart } from "react-icons/gi";
 import ProfileDropDown from "../core/Auth/ProfileDropDown";
 import { apiConnector } from "../../services/apiConnector.service";
 import { categories } from "../../services/apis.service";
-import { IoMdArrowDropdown } from "react-icons/io";
+import { IoMdArrowDropdown,IoMdHome } from "react-icons/io";
+import { CiMenuFries } from "react-icons/ci";
 
 const Navbar = () => {
   const { token } = useSelector((state) => state.auth);
   const { user } = useSelector((state) => state.profile);
   const { totalItems } = useSelector((state) => state.cart);
   const navigate = useNavigate();
+  const location = useLocation();
   const [subLinks, setSubLinks] = useState([]);
+  const [catalog, setCatalog] = useState(false);
+  const [navMenu, setNavMenu] = useState(false);
 
   const fetchSubLinks = async () => {
     try {
@@ -31,52 +35,73 @@ const Navbar = () => {
     fetchSubLinks();
   }, []);
 
-  const location = useLocation();
+  useEffect(() => {
+    setCatalog(false);
+    setNavMenu(false);
+  }, [location.pathname]);
+
   const matchRoute = (route) => {
     return matchPath({ path: route }, location.pathname);
   };
 
+  const handleCatalogClick = () => {
+    if (catalog) setCatalog(false);
+    else setCatalog(true);
+  };
+
+  const handleNavMenuClick = () => {
+    if (navMenu) setNavMenu(false);
+    else setNavMenu(true);
+  };
+
   return (
-    <div className="flex h-14 items-center justify-center border-b-[1px] border-b-richblack-700">
+    <div className="flex flex-wrap h-14 items-center justify-center border-b-[1px] border-b-richblack-700">
       <div className="flex w-11/12 max-w-maxContent items-center justify-between">
         <Link to={"/"}>
-          <img src={logo} width={160} />
+          <img className="w-40 min-w-[160px] flex-shrink-0" src={logo} />
         </Link>
 
         <nav>
           <ul className="flex gap-x-6 text-richblack-25">
             {NavbarLinks.map((link, index) => {
               return (
-                <li key={index}>
+                <li className="hidden md:block" key={index}>
                   {link.title === "Catalog" ? (
                     <div className="relative flex items-center gap-1 group cursor-pointer">
-                      <p className="text-richblack-500 transition-all duration-200 group-hover:text-white">
-                        {link.title}
-                      </p>
-                      <IoMdArrowDropdown className="text-richblack-500 transition-all duration-200 group-hover:text-white" />
+                      <button
+                        className="flex items-center gap-1 group cursor-pointer"
+                        onClick={handleCatalogClick}
+                      >
+                        <p className="text-richblack-500 transition-all duration-200 group-hover:text-white">
+                          {link.title}
+                        </p>
+                        <IoMdArrowDropdown className="text-richblack-500 transition-all duration-200 group-hover:text-white" />
+                      </button>
 
-                      <div className="invisible absolute left-[50%] top-[50%] flex flex-col rounded-md bg-richblack-5 text-richblack-900 opacity-0 transition-all duration-200 group-hover:visible group-hover:opacity-[99%] lg:w-[200px] translate-x-[-50%] translate-y-[12%] z-50 justify-center p-4 group text-[15px] font-medium">
-                        <div className="absolute left-[50%] top-0 h-6 w-6 rotate-45 rounded bg-richblack-5 translate-y-[-30%] translate-x-[85%] -z-10"></div>
-                        {subLinks.length > 0 ? (
-                          subLinks.map((subLink, index) => (
-                            <Link
-                              to={`/catalog/${subLink.name
-                                .split(" ")
-                                .join("-")
-                                .toLowerCase()}`}
-                              key={index}
-                            >
-                              <p className="hover:bg-richblue-25 p-4 rounded-md transition-all duration-200">
-                                {subLink.name}
-                              </p>
-                            </Link>
-                          ))
-                        ) : (
-                          <div className="text-center text-sm text-richblack-400">
-                            No Categories
-                          </div>
-                        )}
-                      </div>
+                      {catalog && (
+                        <div className="absolute left-[50%] top-[60%] flex flex-col rounded-md greenBgShadow transition-all duration-200 md:w-[200px] translate-x-[-50%] translate-y-[12%] z-50 justify-center p-4 group text-lg font-medium">
+                          <div className="absolute left-[50%] -top-[6px] h-6 w-6 rotate-45 rounded bg-richblue-900 border-richblue-300 border-t-2 border-l-2 translate-y-[-30%] translate-x-[85%] -z-50"></div>
+                          {subLinks.length > 0 ? (
+                            subLinks.map((subLink, index) => (
+                              <Link
+                                to={`/catalog/${subLink.name
+                                  .split(" ")
+                                  .join("-")
+                                  .toLowerCase()}`}
+                                key={index}
+                              >
+                                <p className="hover:bg-richblue-600 p-4 rounded-md transition-all duration-200">
+                                  {subLink.name}
+                                </p>
+                              </Link>
+                            ))
+                          ) : (
+                            <div className="text-center text-sm text-richblack-400">
+                              No Categories
+                            </div>
+                          )}
+                        </div>
+                      )}
                     </div>
                   ) : (
                     <Link to={link?.path}>
@@ -99,7 +124,10 @@ const Navbar = () => {
 
         <div className="flex gap-x-4 items-center justify-center">
           {user && user?.accountType !== "instructor" && (
-            <Link to={"/dashboard/cart"} className="relative p-2">
+            <Link
+              to={"/dashboard/cart"}
+              className="relative p-2 hidden md:block"
+            >
               <GiShoppingCart className="transition-all duration-200 scale-150 hover:scale-[1.7]" />
               {totalItems > 0 && (
                 <span className="absolute w-[16px] h-[16px] flex items-center justify-center bottom-4 right-0 text-black bg-yellow-100 rounded-full text-xs font-bold animate-bounce">
@@ -110,15 +138,99 @@ const Navbar = () => {
           )}
 
           {token === null && (
+            <div className="relative text-richblack-100 rounded-md transition-all duration-200 md:hidden">
+              <button className="hover:scale-95" onClick={handleNavMenuClick}>
+                <CiMenuFries className="scale-150" />
+              </button>
+              {navMenu && (
+                <ul className="absolute min-w-[200px] top-12 left-[66%] translate-x-[-66%] z-50 greenBgShadow px-6 py-4 text-lg rounded-lg">
+                  <div className="absolute left-[50%] -top-[6px] h-6 w-6 rotate-45 rounded bg-richblue-900 border-richblue-300 border-t-2 border-l-2 translate-y-[-30%] translate-x-[85%] -z-50"></div>
+                  {NavbarLinks.map((link, index) => {
+                    return (
+                      <li key={index}>
+                        {link.title === "Catalog" ? (
+                          <div className="flex flex-col gap-1 group cursor-pointer">
+                            <button
+                              className="flex items-center gap-1 cursor-pointer"
+                              onClick={handleCatalogClick}
+                            >
+                              <p className="text-richblack-500 transition-all duration-200 group-hover:text-white">
+                                {link.title}
+                              </p>
+                              {catalog ? (
+                                <IoMdArrowDropdown className="text-richblack-500 transition-all duration-200 group-hover:text-white rotate-180" />
+                              ) : (
+                                <IoMdArrowDropdown className="text-richblack-500 transition-all duration-200 group-hover:text-white" />
+                              )}
+                            </button>
+
+                            {catalog && (
+                              <div className="flex flex-col gap-1 transition-all duration-200 justify-center group text-base pb-2">
+                                {subLinks.length > 0 ? (
+                                  subLinks.map((subLink, index) => (
+                                    <Link
+                                      to={`/catalog/${subLink.name
+                                        .split(" ")
+                                        .join("-")
+                                        .toLowerCase()}`}
+                                      key={index}
+                                    >
+                                      <p className="hover:text-white transition-all duration-200 flex gap-1 items-center">
+                                        <IoMdArrowDropdown className="text-richblack-500 transition-all duration-200 group-hover:text-white -rotate-90" />
+                                        {subLink.name}
+                                      </p>
+                                    </Link>
+                                  ))
+                                ) : (
+                                  <div className="text-center text-sm text-richblack-400">
+                                    No Categories
+                                  </div>
+                                )}
+                              </div>
+                            )}
+                          </div>
+                        ) : (
+                          <Link to={link?.path}>
+                            <p
+                              className={`${
+                                matchRoute(link?.path)
+                                  ? "text-yellow-25"
+                                  : "text-richblack-500"
+                              } transition-all duration-200 hover:text-white`}
+                            >
+                              {link.title}
+                            </p>
+                          </Link>
+                        )}
+                      </li>
+                    );
+                  })}
+                  <li>
+                    <Link
+                      to={"/signup"}
+                      className={`${
+                        matchRoute("/signup")
+                          ? "text-yellow-25"
+                          : "text-richblack-500"
+                      } transition-all duration-200 hover:text-white`}
+                    >
+                      Sign Up
+                    </Link>
+                  </li>
+                </ul>
+              )}
+            </div>
+          )}
+          {token === null && (
             <Link to={"/login"}>
-              <button className="border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100 rounded-md transition-all duration-200 hover:scale-95">
+              <button className="border border-richblack-700 bg-richblack-800 min-w-[80px] px-[12px] py-[8px] text-richblack-100 rounded-md transition-all duration-200 hover:scale-95">
                 Log In
               </button>
             </Link>
           )}
           {token === null && (
             <Link to={"/signup"}>
-              <button className="border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100 rounded-md transition-all duration-200 hover:scale-95">
+              <button className="border border-richblack-700 bg-richblack-800 min-w-[80px] px-[12px] py-[8px] text-richblack-100 rounded-md transition-all duration-200 hover:scale-95 hidden md:block">
                 Sign Up
               </button>
             </Link>
--- 08_FullStackProject-1/EduStream/Client/src/components/core/Auth/ProfileDropDown.jsx
+++ 08_FullStackProject-1/EduStream/Client/src/components/core/Auth/ProfileDropDown.jsx
@@ -1,59 +1,191 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useRef, useState } from "react";
 import { AiOutlineCaretDown } from "react-icons/ai";
 import { VscDashboard, VscSignOut } from "react-icons/vsc";
 import { useDispatch, useSelector } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, matchPath, useLocation, useNavigate } from "react-router-dom";
 import ConfirmationModal from "../../common/ConfirmationModal";
 
 import { logout } from "../../../services/operations/authAPI.service";
+import { NavbarLinks } from "../../../data/navbar-links";
+import { apiConnector } from "../../../services/apiConnector.service";
+import { categories } from "../../../services/apis.service";
+import { IoMdArrowDropdown } from "react-icons/io";
+import { GiShoppingCart } from "react-icons/gi";
 
 export default function ProfileDropDown() {
   const { user } = useSelector((state) => state.profile);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const location = useLocation();
   const [confirmationModal, setConfirmationModal] = useState(null);
   const ref = useRef(null);
+  const [subLinks, setSubLinks] = useState([]);
+  const [navMenu, setNavMenu] = useState(false);
+  const [catalog, setCatalog] = useState(false);
+
+  const fetchSubLinks = async () => {
+    try {
+      const result = await apiConnector("GET", categories.CATEGORIES_API);
+      const subLinksArr = await result.data.allCategories;
+      setSubLinks(subLinksArr);
+    } catch (error) {
+      console.log("Could Not Fetch The Category List :- ", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchSubLinks();
+  }, []);
+
+  useEffect(() => {
+    setCatalog(false);
+    setNavMenu(false);
+  }, [location.pathname]);
+
+  const matchRoute = (route) => {
+    return matchPath({ path: route }, location.pathname);
+  };
+
+  const handleNavMenuClick = () => {
+    if (navMenu) setNavMenu(false);
+    else setNavMenu(true);
+  };
+
+  const handleCatalogClick = () => {
+    if (catalog) setCatalog(false);
+    else setCatalog(true);
+  };
 
   return (
-    <button className="realtive group">
-      <div className="flex items-center gap-x-1">
+    <div className="relative">
+      <button
+        className="flex items-center gap-x-1"
+        onClick={handleNavMenuClick}
+      >
         <img
           src={user?.image}
           alt={`profile-${user?.firstName}`}
-          className="aspect-square w-[30px] rounded-full object-cover transition-all duration-200 group-hover:scale-105"
+          className="aspect-square w-8 min-w-[32px] flex-shrink-0 rounded-full object-cover transition-all duration-200 group-hover:scale-105"
         />
         <AiOutlineCaretDown className="text-sm text-richblack-100 transition-all duration-200 group-hover:text-white" />
-      </div>
+      </button>
 
-      <div
-        className="absolute top-[6.5%] right-[9%] z-[1000] divide-y-[1px] divide-richblack-700 overflow-hidden rounded-md border-[1px] border-richblack-700 bg-richblack-800 transition-all duration-200 invisible opacity-0 group group-hover:visible group-hover:opacity-100"
-        ref={ref}
-      >
-        <Link to="/dashboard/my-profile">
-          <div className="flex w-full items-center gap-x-1 py-[10px] px-[12px] text-sm text-richblack-100 hover:bg-richblack-700 hover:text-richblack-25">
-            <VscDashboard className="text-lg" />
-            Dashboard
+      {navMenu && (
+        <>
+          <div className="absolute top-[35px] left-1 h-6 w-6 rotate-45 rounded bg-richblue-900 border-richblue-300 border-t-2 border-l-2 z-[51]"></div>
+          <div
+            className="absolute top-12 -left-[150px] z-50 divide-y-[1px] divide-richblack-700 overflow-hidden rounded-md greenBgShadow transition-all duration-200 min-w-[200px]"
+            ref={ref}
+          >
+            <Link to="/dashboard/my-profile">
+              <div
+                className={`flex w-full items-center gap-x-1 pt-2 pb-1 px-3 text-base transition-all duration-200 ${
+                  matchRoute("/dashboard/my-profile")
+                    ? "text-yellow-25"
+                    : "text-richblack-100"
+                } transition-all duration-200 hover:text-white`}
+              >
+                <VscDashboard className="text-lg" />
+                Dashboard
+              </div>
+            </Link>
+            <Link to="/dashboard/Cart">
+              <div
+                className={`flex md:hidden w-full items-center gap-x-1 pb-2 px-3 text-base transition-all duration-200 ${
+                  matchRoute("/dashboard/Cart")
+                    ? "text-yellow-25"
+                    : "text-richblack-100"
+                } transition-all duration-200 hover:text-white`}
+              >
+                <GiShoppingCart className="text-lg" />
+                Cart
+              </div>
+            </Link>
+            {navMenu && (
+              <ul className="block md:hidden text-richblack-100 p-3">
+                {NavbarLinks.map((link, index) => {
+                  return (
+                    <li key={index}>
+                      {link.title === "Catalog" ? (
+                        <div className="flex flex-col gap-1 group cursor-pointer">
+                          <button
+                            className="flex items-center gap-1 cursor-pointer"
+                            onClick={handleCatalogClick}
+                          >
+                            <p className=" transition-all duration-200 group-hover:text-white">
+                              {link.title}
+                            </p>
+                            {catalog ? (
+                              <IoMdArrowDropdown className=" transition-all duration-200 group-hover:text-white rotate-180" />
+                            ) : (
+                              <IoMdArrowDropdown className=" transition-all duration-200 group-hover:text-white" />
+                            )}
+                          </button>
+
+                          {catalog && (
+                            <div className="flex flex-col gap-1 transition-all duration-200 justify-center group text-base pb-2">
+                              {subLinks.length > 0 ? (
+                                subLinks.map((subLink, index) => (
+                                  <Link
+                                    to={`/catalog/${subLink.name
+                                      .split(" ")
+                                      .join("-")
+                                      .toLowerCase()}`}
+                                    key={index}
+                                  >
+                                    <p className="hover:text-white transition-all duration-200 flex gap-1 items-center">
+                                      <IoMdArrowDropdown className=" transition-all duration-200 group-hover:text-white -rotate-90" />
+                                      {subLink.name}
+                                    </p>
+                                  </Link>
+                                ))
+                              ) : (
+                                <div className="text-center text-sm text-richblack-400">
+                                  No Categories
+                                </div>
+                              )}
+                            </div>
+                          )}
+                        </div>
+                      ) : (
+                        <Link to={link?.path}>
+                          <p
+                            className={`${
+                              matchRoute(link?.path)
+                                ? "text-yellow-25"
+                                : "text-richblack-100"
+                            } transition-all duration-200 hover:text-white`}
+                          >
+                            {link.title}
+                          </p>
+                        </Link>
+                      )}
+                    </li>
+                  );
+                })}
+              </ul>
+            )}
+            <div
+              onClick={() =>
+                setConfirmationModal({
+                  text1: "Are You Sure ?",
+                  text2: "You will be logged out of your account",
+                  btn1Text: "Logout",
+                  btn2Text: "Cancel",
+                  btn1Handler: () => dispatch(logout(navigate)),
+                  btn2Handler: () => setConfirmationModal(null),
+                })
+              }
+              className="flex w-full items-center gap-x-1 py-[10px] px-[12px] text-sm text-richblack-100 hover:text-white cursor-pointer"
+            >
+              <VscSignOut className="text-lg" />
+              <p>Logout</p>
+            </div>
           </div>
-        </Link>
-        <div
-          onClick={() =>
-            setConfirmationModal({
-              text1: "Are You Sure ?",
-              text2: "You will be logged out of your account",
-              btn1Text: "Logout",
-              btn2Text: "Cancel",
-              btn1Handler: () => dispatch(logout(navigate)),
-              btn2Handler: () => setConfirmationModal(null),
-            })
-          }
-          className="flex w-full items-center gap-x-1 py-[10px] px-[12px] text-sm text-richblack-100 hover:bg-richblack-700 hover:text-richblack-25"
-        >
-          <VscSignOut className="text-lg" />
-          Logout
-        </div>
-      </div>
+        </>
+      )}
       {confirmationModal && <ConfirmationModal modalData={confirmationModal} />}
-    </button>
+    </div>
   );
 }
